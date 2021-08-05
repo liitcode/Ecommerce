14c4552be22492ae124c633ce71bdc7519b16528
Divyanshu Verma <69303193+liitcode@users.noreply.github.com>
2021-08-05 18:42:56 +0530

    Product Page and Components

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Loading.scss';
+
+const Loading = () => {
+    return (
+        <main>
+        <div className="loading" />
+        </main>
+    )
+}
+
+export default Loading;
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,28 +1,44 @@
 import React from 'react';
-import './Product.css';
+import './Product.scss';
 import { Link } from 'react-router-dom';
 import ShoppingCartOutlinedIcon  from '@material-ui/icons/ShoppingCartOutlined';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../../redux/actions';
 
 const Product = ({ id, title, image, price, rating, specification, detail}) => {
+    const dispatch = useDispatch();
+    const addItemToCart = () => {
+        const item = {
+            id,
+            title,
+            image,
+            price,
+            rating,
+            specification,
+            detail,
+        };
+        dispatch(addToCart(item));
+    }
+
     return (
         <div className='product'>
-            <div className="info">
-                <Link to = {`/products/${id}`} className='title'>
+            <div className="product-info">
+                <Link to = {`/product/${id}`} className='product-title'>
                     <p>{title}</p>
                 </Link>
-                <p className="price"> 
+                <p className="product-price"> 
                     <strong>$</strong>
                     <strong>{price}</strong>
                 </p>
-                <div className="rating">
+                <div className="product-rating">
                     {Array(rating).fill()
                     .map((_,index) => (
-                        <p key={index}>🌟</p>
+                        <p key={index}>⭐️</p>
                     ) )}
                 </div>
             </div>  
                 <img src = {image} alt='product' />
-                <button>
+                <button onClick={addItemToCart}>
                     <i>
                         <ShoppingCartOutlinedIcon />
                     </i>
